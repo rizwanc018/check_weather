4d4bcfc9a51af881f41e289d40a4f464832b0d4e
rizwanc018 <rizwanc018@gmail.com>
2026-03-26 08:29:50 +0300

    weather api created

---
+++ weather.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_weather(city):
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    print(request_url)
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+
+if __name__ == "__main__":
+    print("\n ** Get current weather ** ")
+    city = input("\nPlease enter city name : ")
+
+    if not bool(city.strip()):
+        city = "Doha"
+
+    weather_data = get_weather(city)
+    pprint(weather_data)
